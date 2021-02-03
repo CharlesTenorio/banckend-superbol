10d39bc1a5fcb199e5910bf3e33fa4831467740e
Charles <charlestenorios@gmail.com>
2021-02-02 21:00:16 -0300

    os lib

--- superbol_api/settings.py
+++ superbol_api/settings.py
@@ -2,6 +2,7 @@ import dj_database_url
 from functools import partial
 from pathlib import Path
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
