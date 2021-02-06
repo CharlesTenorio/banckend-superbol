246fc3f37dc5ab3976b59aac5a8058d59104f7c9
Charles <charlestenorios@gmail.com>
2021-02-05 21:01:05 -0300

    adm gerentes

--- cambistas/admin.py
+++ cambistas/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Cambista
 
 class CambistaAdmin(admin.ModelAdmin):
-    list_display = ('id', 'id_banca', 'nome','cpf')
+    list_display = ('id', 'id_gerente', 'nome','cpf')
     search_fields = ('nome', )
 
 admin.site.register(Cambista, CambistaAdmin)
