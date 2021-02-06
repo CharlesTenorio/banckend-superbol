68d152c39d393ec8e2358404154e40dc97dd8dcc
Charles <charlestenorios@gmail.com>
2021-02-06 19:59:12 -0300

    teste com mocks

--- servicos/tests/test_service.py
+++ servicos/tests/test_service.py
@@ -2,13 +2,13 @@ from unittest.mock import Mock
 
 def test_lista_times():
     time = Mock(return_valeu=200)
-    codigo= time.return_valeu
-    assert codigo.status_code == 200
+    codigo = time.return_valeu
+    assert codigo == 200
 
 def test_lista_ligas():
     time = Mock(return_valeu=200)
-    codigo= time.return_valeu
-    assert codigo.status_code == 200
+    codigo = time.return_valeu
+    assert codigo == 200
 
 
 def test_lista_ligas_tbl():
